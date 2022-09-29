60b8a49db07d8d1565ab4bdd2a292bef81e3479e
majidali129 <itsmajidali129@gmail.com>
2022-09-29 12:50:12 -0700

    Day12.js & index.html added

---
+++ Day12.js
@@ -0,0 +1,30 @@
+// ################ Error handling in JS #################### 
+
+// const throwErrorExampleFun = () => {
+//     let message
+//     let x = prompt('Enter a number: ')
+//     try {
+//       if (x == '') throw 'empty'
+//       if (isNaN(x)) throw 'not a number'
+//       x = Number(x)
+//       if (x < 5) throw 'too low'
+//       if (x > 10) throw 'too high'
+//     } catch (err) {
+//       console.log(err)
+//     }
+//   }
+//   throwErrorExampleFun()
+
+// ############## JS error types ############### 
+//  SyntaxError
+// let square = 2 x 2
+// console.log(square)
+// console.log('Hello, world")
+// let a=2,b=2,
+// square = a*b;
+// console.log(square)
+// console.log("Majid Ali")
+
+// typeError()
+// let num = "Majid Ali"
+// console.log(num.toUpperCase())
